983a3c93e1c101a59ffa2f94b3aeb62104c0e48f
git-songsh <86220586+git-songsh@users.noreply.github.com>
2021-06-23 21:29:51 -0900

    222.py

--- pir2.py
+++ pir2.py
@@ -40,7 +40,6 @@ while True:
         KaKaoURL = 'https://accounts.kakao.com/login/kakaoforbusiness?continue=https://center-pf.kakao.com/'
         ChatRoom = 'https://center-pf.kakao.com/_dLeCs/chats/4812080438129747' #카카오톡챗봇 주소
         options = webdriver.ChromeOptions()
-        #driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
 
  
         #user-agent 변경
@@ -55,25 +54,9 @@ while True:
         #카카오 메인 페이지 로드
         driver.get(KaKaoURL)
         time.sleep(3)
-        
-        #iframes = driver.find_elements_by_tag_name('iframe')
-        #driver.switch_to.frame(len(iframes))
-        #html = driver.page_source
-        #soup = BeautifulSoup(html, 'html.parser')
-        #soup.find()
-        #soup.find_all()
-        #driver.switch_to.default_content()
- 
-        #로그인
-       # driver.execute_script("document.getElementsByName('id')[0].value=\'" + id + "\'")
-        #time.sleep(1)
-        #driver.execute_script("document.getElementsByName('pw')[0].value=\'" + pw + "\'")
-        #time.sleep(1)
-        #driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
-        
+
         driver.find_element_by_id('id_email_2').send_keys(id)
         driver.find_element_by_id('id_password_3').send_keys(pw)
-        
         
         driver.find_element_by_xpath('//*[@id="login-form"]/fieldset/div[8]/button[1]').click()
         time.sleep(3)
@@ -85,13 +68,11 @@ while True:
         
     
         #글 작성
-        driver.find_element_by_id('chatWrite').send_keys('테스트 메시지입니')  #메시지 작성
-        #driver.find_element_by_xpath("//div[@class='box_tf']//button[@class='btn_g btn_submit']//button[@type='button']").click()  #전송버튼
-        driver.find_element_by_xpath("//div[@class='wrap_inp']//button[@type='button']").click()
-        #driver.find_element_by_xpath("//button[@class='btn_g btn_submit']//button[@type='button']").click()
-        #<button class="btn_g btn_submit" type="button">전송</button>
-        #driver.find_element(By.NAME, "q").send_keys("webdriver" + Keys.ENTER)
-  
+        driver.find_element_by_id('chatWrite').send_keys('움직임이 감지되었습니다.')  #메시지 작성
+        time.sleep(3)
+        driver.find_element_by_xpath('//*[@id="kakaoWrap"]/div[1]/div[2]/div/div[2]/div[2]/form/fieldset/button').click()
+        time.sleep(3)
         driver.quit()
+        
         time.sleep(60)
         break
