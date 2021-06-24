756f10da329f7ffde5f2e8f65bafe70f1c7da6c3
git-songsh <86220586+git-songsh@users.noreply.github.com>
2021-06-24 17:07:42 +0900

    Update 2_record.py
    conditions will be specified later

--- 2_record.py
+++ 2_record.py
@@ -6,8 +6,8 @@ def record():
   with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
     now = datetime.datetime.now()
-    filename = now.strftime('')
-    camera.start_recording(output = filename + ' ')
+    filename = now.strftime('%Y-%m-%d %H:%M:%S')
+    camera.start_recording(output = filename + '.h264') #h.264 코덱
     camera.wait_recording(5)
     camera.stop_recording()
     
