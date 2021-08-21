536304aebd0c2012818905155e3cec117307fc6a
git-songsh <86220586+git-songsh@users.noreply.github.com>
2021-08-21 18:40:30 +0900

    import os 부분 추가
    실행 확인 완료

--- record+save.py
+++ record+save.py
@@ -1,5 +1,12 @@
 import cv2
 import subprocess
+import os
+
+if os.path.isfile("qwer.mp4"):
+  os.remove("qwer.mp4")
+  
+if os.path.isfile("qwer.avi"):
+  os.remove("qwer.avi")
 
 cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
@@ -8,8 +15,7 @@ out = cv2.VideoWriter('qwer.avi', fourcc, 25.0, (640, 480))
 while(True):
     ret, frame = cap.read()    # Read 결과와 frame
     if(ret) :
-        #gray = cv2.cvtColor(frame,  cv2.COLOR_BGR2GRAY)    # 입력 받은 화면 Gray로 변환
-        cv2.imshow('frame', frame)    # 컬러 화면 출력        cv2.imshow('frame_gray', gray)    # Gray 화면 출력
+        cv2.imshow('frame', frame)    # 화면 출력 
         out.write(frame)
 
         if cv2.waitKey(1) == ord('q'):
